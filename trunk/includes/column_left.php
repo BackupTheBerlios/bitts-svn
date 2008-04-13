6906e6558f09b7a305658cb3a9b2259b35acb594
e.beukhof <e.beukhof@60fdd794-ec3f-0410-9997-9d1efa82d4ba>
2008-04-13 09:16:08 +0000

    Created & updated basic classes

---
+++ trunk/includes/column_left.php
@@ -0,0 +1,14 @@
+<?php
+/****************************************************************************
+ * CODE FILE   : column_left.php
+ * Project     : BitTS - BART it TimeSheet
+ * Author(s)   : Erwin Beukhof
+ * Date        : 14 april 2008
+ * Description : .....
+ *               .....
+ *               Framework: osCommerce, Open Source E-Commerce Solutions
+ *               http://www.oscommerce.com
+ */
+
+  require(DIR_WS_BOXES . 'main_menu.php');
+?>
