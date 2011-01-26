a1ea7afeaea66193af8e49a2d246cae820c013ee
ebeukhof <ebeukhof@60fdd794-ec3f-0410-9997-9d1efa82d4ba>
2011-01-26 13:51:47 +0000

    First implementation of benefits functionality (not fully developed yet)

--- trunk/includes/activity_entry.php
+++ trunk/includes/activity_entry.php
@@ -3,7 +3,7 @@
  * CODE FILE   : activity_entry.php
  * Project     : BitTS - BART it TimeSheet
  * Author(s)   : Erwin Beukhof
- * Date        : 22 june 2009
+ * Date        : 24 june 2010
  * Description : Activity entry fields
  *               Data validation sequence
  *               Storing of entered data (via timesheet object)
@@ -187,13 +187,21 @@ $_POST['previous_activity_amount'] = activity::format('amount', $_POST['activity
             <td class="item_entry">
               <table border="0" cellspacing="0" cellpadding="2" width="250" class="item_entry">
                 <tr>
-                  <td width="50%" class="item_entry"><?php echo TEXT_ACTIVITY_TRAVELDISTANCE; ?></td><td width="50%" class="item_entry"><?php echo tep_draw_input_field('activity_travel_distance', '', 'size="1" maxlength="5" style="width: 100%"' . ($_POST['action']=='enter_data'||$_POST['action']=='save_data'?'':' disabled')); ?></td>
+                  <td width="50%" class="item_entry"><?php echo TEXT_ACTIVITY_TRAVELDISTANCE; ?></td><td colspan="2" width="50%" class="item_entry"><?php echo tep_draw_input_field('activity_travel_distance', '', 'size="1" maxlength="5" style="width: 100%"' . ($_POST['action']=='enter_data'||$_POST['action']=='save_data'?'':' disabled')); ?></td>
                 </tr>
                 <tr>
-                  <td width="50%" class="item_entry"><?php echo TEXT_ACTIVITY_EXPENSES; ?></td><td width="50%" class="item_entry"><?php echo tep_draw_input_field('activity_expenses', '', 'size="1" maxlength="7" style="width: 100%"' . ($_POST['action']=='enter_data'||$_POST['action']=='save_data'?'':' disabled')); ?></td>
+                  <td width="50%" class="item_entry"><?php echo TEXT_ACTIVITY_EXPENSES; ?></td><td colspan="2" width="50%" class="item_entry"><?php echo tep_draw_input_field('activity_expenses', '', 'size="1" maxlength="7" style="width: 100%"' . ($_POST['action']=='enter_data'||$_POST['action']=='save_data'?'':' disabled')); ?></td>
                 </tr>
                 <tr>
-                  <td width="50%" class="item_entry"><?php echo TEXT_ACTIVITY_TICKETNUMBER; ?></td><td width="50%" class="item_entry"><?php echo tep_draw_input_field('activity_ticket_number', '', 'size="1" maxlength="16" style="width: 100%"' . ($_POST['action']=='enter_data'||$_POST['action']=='save_data'?'':' disabled')); ?></td>
+                  <td width="50%" class="item_entry"><?php echo TEXT_ACTIVITY_TICKETNUMBER; ?></td>
+                  <td width="50%" class="item_entry"><?php echo tep_draw_input_field('activity_ticket_number', '', 'size="1" maxlength="16" style="width: 100%"' . ($_POST['action']=='enter_data'||$_POST['action']=='save_data'?'':' disabled')); ?></td>
+                  <td width="22px" class="item_entry">
+                    <?php if (ENABLE_TICKET_DATABASE_LOOKUPS && ($_POST['action']=='enter_data'||$_POST['action']=='save_data')) {
+                      echo tep_image(DIR_WS_LANGUAGES . $_SESSION['language'] . '/images/buttons/button_lookup.gif');
+                    } else {
+                      echo tep_image(DIR_WS_LANGUAGES . $_SESSION['language'] . '/images/buttons/button_lookup_disabled.gif');
+                    } ?>
+                  </td>
                 </tr>
               </table>
             </td>
@@ -203,7 +211,8 @@ $_POST['previous_activity_amount'] = activity::format('amount', $_POST['activity
               <?php echo TEXT_ACTIVITY_COMMENT; ?>
             </td>
             <td class="item_entry">
-              <?php echo tep_draw_input_field('activity_comment', '', 'size="1" maxlength="64" style="width: 100%"' . ($_POST['action']=='enter_data'||$_POST['action']=='save_data'?'':' disabled')); ?>
+              <?php //echo tep_draw_input_field('activity_comment', '', 'size="1" maxlength="256" style="width: 100%"' . ($_POST['action']=='enter_data'||$_POST['action']=='save_data'?'':' disabled'));
+                echo tep_draw_textarea_field('activity_comment', 'off', 64, 4, '', 'onKeyUp="maxLength(this, 256)" style="width: 100%"' . ($_POST['action']=='enter_data'||$_POST['action']=='save_data'?'':' disabled')); ?>
             </td>
           </tr>
           <tr>
