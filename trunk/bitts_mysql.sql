994f1d78cbcbaa71b60d5f89a8af7e2c54d09a6e
ebeukhof <ebeukhof@60fdd794-ec3f-0410-9997-9d1efa82d4ba>
2011-01-26 13:50:14 +0000

    First implementation of benefits functionality (not fully developed yet)

--- trunk/bitts_mysql.sql
+++ trunk/bitts_mysql.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: bitts
 -- ------------------------------------------------------
--- Server version	5.0.32-Debian_7etch6-log
+-- Server version	5.0.51a-24+lenny4
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,44 +20,102 @@
 --
 
 DROP TABLE IF EXISTS `activities`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `activities` (
-  `activities_id` int(11) NOT NULL auto_increment,
+  `activities_id` int(10) unsigned NOT NULL auto_increment,
   `activities_date` date NOT NULL default '0000-00-00',
-  `tariffs_id` int(11) NOT NULL default '0',
+  `tariffs_id` int(10) unsigned NOT NULL default '0',
   `activities_amount` decimal(5,2) NOT NULL default '0.00',
   `activities_travel_distance` int(11) NOT NULL default '0',
   `activities_expenses` decimal(6,2) NOT NULL default '0.00',
   `activities_ticket_number` varchar(16) NOT NULL default '',
-  `activities_comment` varchar(64) NOT NULL default '',
-  `timesheets_id` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`activities_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=287 DEFAULT CHARSET=latin1;
+  `activities_comment` varchar(256) NOT NULL default '',
+  `timesheets_id` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`activities_id`),
+  KEY `tariffs_id` (`tariffs_id`),
+  KEY `timesheets_id` (`timesheets_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3090 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `benefits`
+--
+
+DROP TABLE IF EXISTS `benefits`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `benefits` (
+  `benefits_id` int(10) unsigned NOT NULL auto_increment,
+  `benefits_start_date` date NOT NULL default '0000-00-00',
+  `benefits_end_date` date NOT NULL default '0000-00-00',
+  `benefits_credit` decimal(5,2) NOT NULL default '0.00',
+  `benefits_granted` decimal(5,2) NOT NULL default '0.00',
+  `benefits_comment` varchar(256) NOT NULL default '',
+  `employees_id` int(10) unsigned NOT NULL default '0',
+  `roles_id` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`benefits_id`),
+  KEY `employees_id` (`employees_id`),
+  KEY `roles_id` (`roles_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `business_units`
 --
 
 DROP TABLE IF EXISTS `business_units`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `business_units` (
-  `business_units_id` int(11) NOT NULL auto_increment,
+  `business_units_id` int(10) unsigned NOT NULL auto_increment,
   `business_units_name` varchar(64) NOT NULL default '',
   `business_units_image` varchar(32) default NULL,
+  `business_units_image_position` char(1) default '',
   PRIMARY KEY  (`business_units_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `categories` (
+  `categories_id` int(10) unsigned NOT NULL auto_increment,
+  `categories_name` varchar(64) NOT NULL,
+  PRIMARY KEY  (`categories_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Algemeen'),(2,'Beheer'),(3,'Intern'),(4,'Verlof'),(5,'Ziek'),(6,'Studie'),(7,'Arts'),(8,'Sales'),(9,'BijzonderVerlof'),(10,'Secretariaat'),(11,'ReistijdOverdag'),(12,'CompensatieOverwerk'),(13,'OnderhoudsContract');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `configuration`
 --
 
 DROP TABLE IF EXISTS `configuration`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `configuration` (
-  `configuration_id` int(11) NOT NULL auto_increment,
+  `configuration_id` int(10) unsigned NOT NULL auto_increment,
   `configuration_title` varchar(64) NOT NULL default '',
   `configuration_key` varchar(64) NOT NULL default '',
   `configuration_value` varchar(255) NOT NULL default '',
   `configuration_description` varchar(255) NOT NULL default '',
   PRIMARY KEY  (`configuration_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Dumping data for table `configuration`
@@ -65,7 +123,7 @@ CREATE TABLE `configuration` (
 
 LOCK TABLES `configuration` WRITE;
 /*!40000 ALTER TABLE `configuration` DISABLE KEYS */;
-INSERT INTO `configuration` VALUES (1,'Company Name','COMPANY_NAME','BART it B.V.','The Name of the Company'),(3,'Company Banner','COMPANY_BANNER','logo_Bart-it_156x71.jpg','The Banner of the Company');
+INSERT INTO `configuration` VALUES (1,'Company Name','COMPANY_NAME','BART it B.V.','The Name of the Company'),(3,'Company Banner','COMPANY_BANNER','logo_Bart-it_156x71.jpg','The Banner of the Company'),(4,'Default Language','DEFAULT_LANGUAGE','nl','Default Language'),(5,'Heading Image Width','HEADING_IMAGE_WIDTH','64','Heading Image Width'),(6,'Heading Image Height','HEADING_IMAGE_HEIGHT','64','Heading Image Height'),(7,'Minimum hours per day','MINIMUM_HOURS_PER_DAY','8','The minimum amount of hours that has to be administered per weekday'),(8,'Benefits Leave Role','BENEFITS_LEAVE_ROLE','8','The role that represents Leave under category Benefits');
 /*!40000 ALTER TABLE `configuration` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -74,8 +132,10 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `customers`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `customers` (
-  `customers_id` int(11) NOT NULL default '0',
+  `customers_id` int(10) unsigned NOT NULL default '0',
   `customers_name` varchar(64) NOT NULL default '',
   `customers_billing_name1` varchar(64) NOT NULL default '',
   `customers_billing_name2` varchar(64) NOT NULL default '',
@@ -88,50 +148,61 @@ CREATE TABLE `customers` (
   `customers_billing_fax` varchar(32) NOT NULL default '',
   PRIMARY KEY  (`customers_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `employees`
 --
 
 DROP TABLE IF EXISTS `employees`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `employees` (
-  `employees_id` int(11) NOT NULL default '0',
+  `employees_id` int(10) unsigned NOT NULL default '0',
   `employees_login` varchar(16) NOT NULL default '',
   `employees_fullname` varchar(64) NOT NULL default '',
   `employees_password` char(41) NOT NULL default '',
-  `employees_is_user` tinyint(1) NOT NULL default '0',
-  `employees_is_analyst` tinyint(1) NOT NULL default '0',
-  `employees_is_administrator` tinyint(1) NOT NULL default '0',
-  PRIMARY KEY  (`employees_id`)
+  `profiles_id` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`employees_id`),
+  KEY `profiles_id` (`profiles_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `employees_roles`
 --
 
 DROP TABLE IF EXISTS `employees_roles`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `employees_roles` (
-  `employees_roles_id` int(11) NOT NULL auto_increment,
+  `employees_roles_id` int(10) unsigned NOT NULL auto_increment,
   `employees_roles_start_date` date NOT NULL default '0000-00-00',
   `employees_roles_end_date` date NOT NULL default '0000-00-00',
-  `roles_id` int(11) NOT NULL default '0',
-  `employees_id` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`employees_roles_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=270 DEFAULT CHARSET=latin1;
+  `roles_id` int(10) unsigned NOT NULL default '0',
+  `employees_id` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`employees_roles_id`),
+  KEY `roles_id` (`roles_id`),
+  KEY `employees_id` (`employees_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=423 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `languages`
 --
 
 DROP TABLE IF EXISTS `languages`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `languages` (
-  `languages_id` int(11) NOT NULL auto_increment,
+  `languages_id` int(10) unsigned NOT NULL auto_increment,
   `languages_name` varchar(32) NOT NULL default '',
   `languages_code` char(2) NOT NULL default '',
   `languages_image` varchar(32) default NULL,
   `languages_directory` varchar(32) default NULL,
   PRIMARY KEY  (`languages_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Dumping data for table `languages`
@@ -144,12 +215,43 @@ INSERT INTO `languages` VALUES (1,'Nederlands','nl','icon.gif','dutch');
 UNLOCK TABLES;
 
 --
+-- Table structure for table `profiles`
+--
+
+DROP TABLE IF EXISTS `profiles`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `profiles` (
+  `profiles_id` int(10) unsigned NOT NULL auto_increment,
+  `profiles_name` varchar(32) NOT NULL default '',
+  `profiles_rights_login` tinyint(1) NOT NULL default '0',
+  `profiles_rights_projectlisting` tinyint(1) NOT NULL default '0',
+  `profiles_rights_timeregistration` tinyint(1) NOT NULL default '0',
+  `profiles_rights_analysis` tinyint(1) NOT NULL default '0',
+  `profiles_rights_administration` tinyint(1) NOT NULL default '0',
+  PRIMARY KEY  (`profiles_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `profiles`
+--
+
+LOCK TABLES `profiles` WRITE;
+/*!40000 ALTER TABLE `profiles` DISABLE KEYS */;
+INSERT INTO `profiles` VALUES (5,'Beheerder',1,1,1,1,1),(4,'Rapporteur',1,1,1,1,0),(3,'Medewerker (tijdelijk dv)',1,0,1,0,0),(2,'Medewerker (vast dv)',1,1,1,0,0),(1,'Geen (uit dienst)',0,0,0,0,0);
+/*!40000 ALTER TABLE `profiles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `projects`
 --
 
 DROP TABLE IF EXISTS `projects`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `projects` (
-  `projects_id` int(11) NOT NULL auto_increment,
+  `projects_id` int(10) unsigned NOT NULL auto_increment,
   `projects_name` varchar(64) NOT NULL default '',
   `projects_description` varchar(255) NOT NULL default '',
   `projects_customers_contact_name` varchar(64) NOT NULL default '',
@@ -157,76 +259,129 @@ CREATE TABLE `projects` (
   `projects_start_date` date NOT NULL default '0000-00-00',
   `projects_end_date` date NOT NULL default '0000-00-00',
   `projects_calculated_hours` int(11) NOT NULL default '0',
-  `business_units_id` int(11) NOT NULL default '0',
-  `customers_id` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`projects_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+  `projects_calculated_hours_period` char(1) NOT NULL default 'E',
+  `business_units_id` int(10) unsigned NOT NULL default '0',
+  `customers_id` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`projects_id`),
+  KEY `customers_id` (`customers_id`),
+  KEY `business_units_id` (`business_units_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=51 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `roles`
 --
 
 DROP TABLE IF EXISTS `roles`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `roles` (
-  `roles_id` int(11) NOT NULL auto_increment,
+  `roles_id` int(10) unsigned NOT NULL auto_increment,
   `roles_name` varchar(64) NOT NULL default '',
   `roles_description` varchar(255) NOT NULL default '',
   `roles_mandatory_ticket_entry` tinyint(1) NOT NULL default '0',
-  `projects_id` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`roles_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
+  `projects_id` int(10) unsigned NOT NULL default '0',
+  `categories_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`roles_id`),
+  KEY `projects_id` (`projects_id`),
+  KEY `categories_id` (`categories_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=86 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `sessions`
 --
 
 DROP TABLE IF EXISTS `sessions`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `sessions` (
   `sessions_key` varchar(32) NOT NULL default '',
   `sessions_expiry` int(10) unsigned NOT NULL default '0',
   `sessions_value` text NOT NULL,
   PRIMARY KEY  (`sessions_key`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `tariffs`
 --
 
 DROP TABLE IF EXISTS `tariffs`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `tariffs` (
-  `tariffs_id` int(11) NOT NULL auto_increment,
+  `tariffs_id` int(10) unsigned NOT NULL auto_increment,
   `tariffs_amount` decimal(5,2) NOT NULL default '0.00',
-  `units_id` int(11) NOT NULL default '0',
-  `employees_roles_id` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`tariffs_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=588 DEFAULT CHARSET=latin1;
+  `tariffs_start_date` date NOT NULL default '0000-00-00',
+  `tariffs_end_date` date NOT NULL default '0000-00-00',
+  `units_id` int(10) unsigned NOT NULL default '0',
+  `employees_roles_id` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`tariffs_id`),
+  KEY `units_id` (`units_id`),
+  KEY `employees_roles_id` (`employees_roles_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1634 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `timesheets`
 --
 
 DROP TABLE IF EXISTS `timesheets`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `timesheets` (
-  `timesheets_id` int(11) NOT NULL auto_increment,
+  `timesheets_id` int(10) unsigned NOT NULL auto_increment,
   `timesheets_start_date` date NOT NULL default '0000-00-00',
   `timesheets_end_date` date NOT NULL default '0000-00-00',
   `timesheets_locked` tinyint(1) NOT NULL default '0',
-  `employees_id` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`timesheets_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+  `employees_id` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`timesheets_id`),
+  KEY `employees_id` (`employees_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=98 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `units`
 --
 
 DROP TABLE IF EXISTS `units`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `units` (
-  `units_id` int(11) NOT NULL auto_increment,
+  `units_id` int(10) unsigned NOT NULL auto_increment,
   `units_name` varchar(64) NOT NULL default '',
   `units_description` varchar(255) NOT NULL default '',
   PRIMARY KEY  (`units_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
+--
+-- Temporary table structure for view `v_benefits`
+--
+
+DROP TABLE IF EXISTS `v_benefits`;
+/*!50001 DROP VIEW IF EXISTS `v_benefits`*/;
+/*!50001 CREATE TABLE `v_benefits` (
+  `employees_id` int(10) unsigned,
+  `benefits_start_date` date,
+  `benefits_end_date` date,
+  `roles_id` int(10) unsigned,
+  `benefits_credit` decimal(5,2),
+  `benefits_granted` decimal(5,2),
+  `benefits_comment` varchar(256),
+  `benefits_used` decimal(27,2)
+) */;
+
+--
+-- Final view structure for view `v_benefits`
+--
+
+/*!50001 DROP TABLE `v_benefits`*/;
+/*!50001 DROP VIEW IF EXISTS `v_benefits`*/;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`192.168.0.%` SQL SECURITY DEFINER */
+/*!50001 VIEW `v_benefits` AS select `benefits`.`employees_id` AS `employees_id`,`benefits`.`benefits_start_date` AS `benefits_start_date`,`benefits`.`benefits_end_date` AS `benefits_end_date`,`benefits`.`roles_id` AS `roles_id`,`benefits`.`benefits_credit` AS `benefits_credit`,`benefits`.`benefits_granted` AS `benefits_granted`,`benefits`.`benefits_comment` AS `benefits_comment`,ifnull(sum(`activities`.`activities_amount`),0.00) AS `benefits_used` from (`benefits` left join (((`activities` join `timesheets`) join `tariffs`) join `employees_roles`) on(((`benefits`.`employees_id` = `timesheets`.`employees_id`) and (`timesheets`.`timesheets_id` = `activities`.`timesheets_id`) and (`activities`.`tariffs_id` = `tariffs`.`tariffs_id`) and (`tariffs`.`employees_roles_id` = `employees_roles`.`employees_roles_id`) and (`benefits`.`roles_id` = `employees_roles`.`roles_id`) and (`activities`.`activities_date` >= `benefits`.`benefits_start_date`) and (`activities`.`activities_date` <= `benefits`.`benefits_end_date`)))) group by `benefits`.`employees_id`,`benefits`.`benefits_start_date`,`benefits`.`benefits_end_date` */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -237,4 +392,4 @@ CREATE TABLE `units` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2008-09-18 20:45:01
+-- Dump completed on 2011-01-25 22:55:02
