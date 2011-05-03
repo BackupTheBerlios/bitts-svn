0c038dab048563e405e03a4b2ab7f598d6a61904
ebeukhof <ebeukhof@60fdd794-ec3f-0410-9997-9d1efa82d4ba>
2011-05-03 10:59:55 +0000

    Altered benefits view

--- trunk/bitts_mysql.sql
+++ trunk/bitts_mysql.sql
@@ -1,9 +1,3 @@
--- MySQL dump 10.11
---
--- Host: localhost    Database: bitts
--- ------------------------------------------------------
--- Server version	5.0.51a-24+lenny4
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -30,12 +24,12 @@ CREATE TABLE `activities` (
   `activities_travel_distance` int(11) NOT NULL default '0',
   `activities_expenses` decimal(6,2) NOT NULL default '0.00',
   `activities_ticket_number` varchar(16) NOT NULL default '',
-  `activities_comment` varchar(256) NOT NULL default '',
+  `activities_comment` varchar(255) NOT NULL default '',
   `timesheets_id` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`activities_id`),
   KEY `tariffs_id` (`tariffs_id`),
   KEY `timesheets_id` (`timesheets_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3090 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=8991 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -57,7 +51,7 @@ CREATE TABLE `benefits` (
   PRIMARY KEY  (`benefits_id`),
   KEY `employees_id` (`employees_id`),
   KEY `roles_id` (`roles_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -73,7 +67,7 @@ CREATE TABLE `business_units` (
   `business_units_image` varchar(32) default NULL,
   `business_units_image_position` char(1) default '',
   PRIMARY KEY  (`business_units_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -87,18 +81,8 @@ CREATE TABLE `categories` (
   `categories_id` int(10) unsigned NOT NULL auto_increment,
   `categories_name` varchar(64) NOT NULL,
   PRIMARY KEY  (`categories_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 SET character_set_client = @saved_cs_client;
-
---
--- Dumping data for table `categories`
---
-
-LOCK TABLES `categories` WRITE;
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,'Algemeen'),(2,'Beheer'),(3,'Intern'),(4,'Verlof'),(5,'Ziek'),(6,'Studie'),(7,'Arts'),(8,'Sales'),(9,'BijzonderVerlof'),(10,'Secretariaat'),(11,'ReistijdOverdag'),(12,'CompensatieOverwerk'),(13,'OnderhoudsContract');
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `configuration`
@@ -123,7 +107,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `configuration` WRITE;
 /*!40000 ALTER TABLE `configuration` DISABLE KEYS */;
-INSERT INTO `configuration` VALUES (1,'Company Name','COMPANY_NAME','BART it B.V.','The Name of the Company'),(3,'Company Banner','COMPANY_BANNER','logo_Bart-it_156x71.jpg','The Banner of the Company'),(4,'Default Language','DEFAULT_LANGUAGE','nl','Default Language'),(5,'Heading Image Width','HEADING_IMAGE_WIDTH','64','Heading Image Width'),(6,'Heading Image Height','HEADING_IMAGE_HEIGHT','64','Heading Image Height'),(7,'Minimum hours per day','MINIMUM_HOURS_PER_DAY','8','The minimum amount of hours that has to be administered per weekday'),(8,'Benefits Leave Role','BENEFITS_LEAVE_ROLE','8','The role that represents Leave under category Benefits');
+INSERT INTO `configuration` VALUES (1,'Company Name','COMPANY_NAME','BART it B.V.','The Name of the Company'),(3,'Company Banner','COMPANY_BANNER','logo_Bart-it_156x71.jpg','The Banner of the Company'),(4,'Default Language','DEFAULT_LANGUAGE','nl','Default Language'),(5,'Heading Image Width','HEADING_IMAGE_WIDTH','64','Heading Image Width'),(6,'Heading Image Height','HEADING_IMAGE_HEIGHT','64','Heading Image Height'),(7,'Minimum Hours Per Day','MINIMUM_HOURS_PER_DAY','8','Minimum amount of hours that have to be administered per day'),(8,'Benefits Leave Role','BENEFITS_LEAVE_ROLE','8','The role that represents Leave under category Benefits');
 /*!40000 ALTER TABLE `configuration` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -182,9 +166,9 @@ CREATE TABLE `employees_roles` (
   `roles_id` int(10) unsigned NOT NULL default '0',
   `employees_id` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`employees_roles_id`),
-  KEY `roles_id` (`roles_id`),
-  KEY `employees_id` (`employees_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=423 DEFAULT CHARSET=latin1;
+  KEY `employees_id` (`employees_id`),
+  KEY `roles_id` (`roles_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=598 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -230,7 +214,7 @@ CREATE TABLE `profiles` (
   `profiles_rights_analysis` tinyint(1) NOT NULL default '0',
   `profiles_rights_administration` tinyint(1) NOT NULL default '0',
   PRIMARY KEY  (`profiles_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -263,9 +247,9 @@ CREATE TABLE `projects` (
   `business_units_id` int(10) unsigned NOT NULL default '0',
   `customers_id` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`projects_id`),
-  KEY `customers_id` (`customers_id`),
-  KEY `business_units_id` (`business_units_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=51 DEFAULT CHARSET=latin1;
+  KEY `business_units_id` (`business_units_id`),
+  KEY `customers_id` (`customers_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=66 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -283,9 +267,9 @@ CREATE TABLE `roles` (
   `projects_id` int(10) unsigned NOT NULL default '0',
   `categories_id` int(10) unsigned NOT NULL,
   PRIMARY KEY  (`roles_id`),
-  KEY `projects_id` (`projects_id`),
-  KEY `categories_id` (`categories_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=86 DEFAULT CHARSET=latin1;
+  KEY `categories_id` (`categories_id`),
+  KEY `projects_id` (`projects_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=121 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -318,9 +302,9 @@ CREATE TABLE `tariffs` (
   `units_id` int(10) unsigned NOT NULL default '0',
   `employees_roles_id` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`tariffs_id`),
-  KEY `units_id` (`units_id`),
-  KEY `employees_roles_id` (`employees_roles_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1634 DEFAULT CHARSET=latin1;
+  KEY `employees_roles_id` (`employees_roles_id`),
+  KEY `units_id` (`units_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2320 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -338,7 +322,7 @@ CREATE TABLE `timesheets` (
   `employees_id` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`timesheets_id`),
   KEY `employees_id` (`employees_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=98 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=282 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -353,7 +337,7 @@ CREATE TABLE `units` (
   `units_name` varchar(64) NOT NULL default '',
   `units_description` varchar(255) NOT NULL default '',
   PRIMARY KEY  (`units_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -374,6 +358,29 @@ DROP TABLE IF EXISTS `v_benefits`;
 ) */;
 
 --
+-- Temporary table structure for view `v_projects_total`
+--
+
+DROP TABLE IF EXISTS `v_projects_total`;
+/*!50001 DROP VIEW IF EXISTS `v_projects_total`*/;
+/*!50001 CREATE TABLE `v_projects_total` (
+  `projects_start_date` date,
+  `projects_end_date` date,
+  `projects_name` varchar(64),
+  `categories_name` varchar(64),
+  `roles_name` varchar(64),
+  `employees_roles_start_date` date,
+  `employees_roles_end_date` date,
+  `employees_fullname` varchar(64),
+  `units_name` varchar(64),
+  `tariffs_amount` decimal(5,2),
+  `tariffs_start_date` date,
+  `tariffs_end_date` date,
+  `units_id` int(10) unsigned,
+  `employees_roles_id` int(10) unsigned
+) */;
+
+--
 -- Final view structure for view `v_benefits`
 --
 
@@ -381,7 +388,17 @@ DROP TABLE IF EXISTS `v_benefits`;
 /*!50001 DROP VIEW IF EXISTS `v_benefits`*/;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`root`@`192.168.0.%` SQL SECURITY DEFINER */
-/*!50001 VIEW `v_benefits` AS select `benefits`.`employees_id` AS `employees_id`,`benefits`.`benefits_start_date` AS `benefits_start_date`,`benefits`.`benefits_end_date` AS `benefits_end_date`,`benefits`.`roles_id` AS `roles_id`,`benefits`.`benefits_credit` AS `benefits_credit`,`benefits`.`benefits_granted` AS `benefits_granted`,`benefits`.`benefits_comment` AS `benefits_comment`,ifnull(sum(`activities`.`activities_amount`),0.00) AS `benefits_used` from (`benefits` left join (((`activities` join `timesheets`) join `tariffs`) join `employees_roles`) on(((`benefits`.`employees_id` = `timesheets`.`employees_id`) and (`timesheets`.`timesheets_id` = `activities`.`timesheets_id`) and (`activities`.`tariffs_id` = `tariffs`.`tariffs_id`) and (`tariffs`.`employees_roles_id` = `employees_roles`.`employees_roles_id`) and (`benefits`.`roles_id` = `employees_roles`.`roles_id`) and (`activities`.`activities_date` >= `benefits`.`benefits_start_date`) and (`activities`.`activities_date` <= `benefits`.`benefits_end_date`)))) group by `benefits`.`employees_id`,`benefits`.`benefits_start_date`,`benefits`.`benefits_end_date` */;
+/*!50001 VIEW `v_benefits` AS select `benefits`.`employees_id` AS `employees_id`,`benefits`.`benefits_id` AS `benefits_id`,`benefits`.`benefits_start_date` AS `benefits_start_date`,`benefits`.`benefits_end_date` AS `benefits_end_date`,`benefits`.`roles_id` AS `roles_id`,`benefits`.`benefits_credit` AS `benefits_credit`,`benefits`.`benefits_granted` AS `benefits_granted`,`benefits`.`benefits_comment` AS `benefits_comment`,ifnull(sum(`activities`.`activities_amount`),0.00) AS `benefits_used` from (`benefits` left join (((`activities` join `timesheets`) join `tariffs`) join `employees_roles`) on(((`benefits`.`employees_id` = `timesheets`.`employees_id`) and (`timesheets`.`timesheets_id` = `activities`.`timesheets_id`) and (`activities`.`tariffs_id` = `tariffs`.`tariffs_id`) and (`tariffs`.`employees_roles_id` = `employees_roles`.`employees_roles_id`) and (`benefits`.`roles_id` = `employees_roles`.`roles_id`) and (`activities`.`activities_date` >= `benefits`.`benefits_start_date`) and (`activities`.`activities_date` <= `benefits`.`benefits_end_date`)))) group by `benefits`.`employees_id`,`benefits`.`benefits_id`,`benefits`.`benefits_start_date`,`benefits`.`benefits_end_date` */;
+
+--
+-- Final view structure for view `v_projects_total`
+--
+
+/*!50001 DROP TABLE `v_projects_total`*/;
+/*!50001 DROP VIEW IF EXISTS `v_projects_total`*/;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`192.168.0.%` SQL SECURITY DEFINER */
+/*!50001 VIEW `v_projects_total` AS select `projects`.`projects_start_date` AS `projects_start_date`,`projects`.`projects_end_date` AS `projects_end_date`,`projects`.`projects_name` AS `projects_name`,`categories`.`categories_name` AS `categories_name`,`roles`.`roles_name` AS `roles_name`,`employees_roles`.`employees_roles_start_date` AS `employees_roles_start_date`,`employees_roles`.`employees_roles_end_date` AS `employees_roles_end_date`,`employees`.`employees_fullname` AS `employees_fullname`,`units`.`units_name` AS `units_name`,`tariffs`.`tariffs_amount` AS `tariffs_amount`,`tariffs`.`tariffs_start_date` AS `tariffs_start_date`,`tariffs`.`tariffs_end_date` AS `tariffs_end_date`,`units`.`units_id` AS `units_id`,`employees_roles`.`employees_roles_id` AS `employees_roles_id` from ((`projects` join (((`categories` join `roles`) join `employees`) join `employees_roles`) on(((`projects`.`projects_id` = `roles`.`projects_id`) and (`categories`.`categories_id` = `roles`.`categories_id`) and (`roles`.`roles_id` = `employees_roles`.`roles_id`) and (`employees`.`employees_id` = `employees_roles`.`employees_id`)))) left join (`tariffs` join `units`) on(((`employees_roles`.`employees_roles_id` = `tariffs`.`employees_roles_id`) and (`units`.`units_id` = `tariffs`.`units_id`)))) */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -391,5 +408,3 @@ DROP TABLE IF EXISTS `v_benefits`;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2011-01-25 22:55:02
