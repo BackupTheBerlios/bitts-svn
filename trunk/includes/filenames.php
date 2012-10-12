127a173927e0f6a7d98134a95b696056d98e82cc
ebeukhof <ebeukhof@60fdd794-ec3f-0410-9997-9d1efa82d4ba>
2012-10-12 09:03:44 +0000

    Preparation for punch clock

--- trunk/includes/filenames.php
+++ trunk/includes/filenames.php
@@ -3,7 +3,7 @@
  * CODE FILE   : filenames.php
  * Project     : BitTS - BART it TimeSheet
  * Author(s)   : Erwin Beukhof
- * Date        : 25 january 2011
+ * Date        : 12 october 2012
  * Description : Definitions of filenames
  * 
  *               Framework: osCommerce, Open Source E-Commerce Solutions
@@ -16,6 +16,7 @@
   define('FILENAME_SSL_CHECK', 'ssl_check.php');
   define('FILENAME_TIMEREGISTRATION', 'timeregistration.php');
   define('FILENAME_TIMEREGISTRATION_CALENDAR', 'timeregistration_calendar.php');
+  define('FILENAME_PUNCH_CLOCK', 'punch_clock.php');
   define('FILENAME_ANALYSIS', 'analysis.php');
   define('FILENAME_REPORT', 'report.php');
   define('FILENAME_EXPORT', 'export.php');
