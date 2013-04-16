c4f0f17883b2ef66f82b334a79fa047ec2055769
ebeukhof <ebeukhof@60fdd794-ec3f-0410-9997-9d1efa82d4ba>
2013-04-16 12:52:20 +0000

    Added simple html footer for dialogs

---
+++ trunk/includes/footer_dialog.php
@@ -0,0 +1,16 @@
+<?php
+/****************************************************************************
+ * CODE FILE   : footer_dialog.php
+ * Project     : BitTS - BART it TimeSheet
+ * Author(s)   : Erwin Beukhof
+ * Date        : 16 april 2013
+ * Description : Simple html footer for dialogs.
+ */
+?>
+    <table border="0" width="100%" cellspacing="0" cellpadding="1">
+      <tr class="footer">
+        <td class="footer">&nbsp;</td>
+      </tr>
+    </table>
+  </body>
+</html>
