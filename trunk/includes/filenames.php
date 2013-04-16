bf61035ed128a9b7f155c89c38c926263ce43bb4
ebeukhof <ebeukhof@60fdd794-ec3f-0410-9997-9d1efa82d4ba>
2013-04-16 10:20:53 +0000

    Altered ticket number and description lookup

--- trunk/includes/filenames.php
+++ trunk/includes/filenames.php
@@ -3,7 +3,7 @@
  * CODE FILE   : filenames.php
  * Project     : BitTS - BART it TimeSheet
  * Author(s)   : Erwin Beukhof
- * Date        : 12 october 2012
+ * Date        : 16 april 2013
  * Description : Definitions of filenames
  * 
  *               Framework: osCommerce, Open Source E-Commerce Solutions
@@ -17,6 +17,7 @@
   define('FILENAME_TIMEREGISTRATION', 'timeregistration.php');
   define('FILENAME_TIMEREGISTRATION_CALENDAR', 'timeregistration_calendar.php');
   define('FILENAME_TIMEREGISTRATION_PUNCH_CLOCK', 'timeregistration_punch_clock.php');
+  define('FILENAME_TIMEREGISTRATION_TICKET_LOOKUP', 'timeregistration_ticket_lookup.php');
   define('FILENAME_ANALYSIS', 'analysis.php');
   define('FILENAME_REPORT', 'report.php');
   define('FILENAME_EXPORT', 'export.php');
