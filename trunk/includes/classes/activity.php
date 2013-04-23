56641b466538d51649b4fd67e75f98ecab231634
ebeukhof <ebeukhof@60fdd794-ec3f-0410-9997-9d1efa82d4ba>
2013-04-23 12:51:48 +0000

    Added travel description column

--- trunk/includes/classes/activity.php
+++ trunk/includes/classes/activity.php
@@ -3,13 +3,13 @@
  * CLASS FILE  : activity.php
  * Project     : BitTS - BART it TimeSheet
  * Author(s)   : Erwin Beukhof
- * Date        : 01 march 2013
+ * Date        : 23 april 2013
  * Description : Activity class
  *
  */
 
   class activity {
-    private $activity_id, $date, $amount, $tariff, $travel_distance, $expenses, $ticket_number, $comment, $timesheet_id;
+    private $activity_id, $date, $amount, $tariff, $travel_distance, $travel_description, $expenses, $ticket_number, $comment, $timesheet_id;
 
     public function __construct($activity_id = 0) {
       $database = $_SESSION['database'];
@@ -18,7 +18,17 @@
       if ($this->activity_id != 0) {
         $this->activity_id = $database->prepare_input($this->activity_id);
 
-        $activity_query = $database->query("select activities_date, tariffs_id, activities_amount, activities_travel_distance, activities_expenses, activities_ticket_number, activities_comment, timesheets_id from " . TABLE_ACTIVITIES . " where activities_id = '" . (int)$this->activity_id . "'");
+        $activity_query = $database->query("SELECT activities_date, " .
+                                             "tariffs_id, " .
+                                             "activities_amount, " .
+                                             "activities_travel_distance, " .
+                                             "activities_travel_description, " .
+                                             "activities_expenses, " .
+                                             "activities_ticket_number, " .
+                                             "activities_comment, " .
+                                             "timesheets_id " .
+                                           "FROM " . TABLE_ACTIVITIES . " " .
+                                           "WHERE activities_id = '" . (int)$this->activity_id . "';");
         $activity_result = $database->fetch_array($activity_query);
 
         if (tep_not_null($activity_result)) {
@@ -27,6 +37,7 @@
                       $activity_result['activities_amount'],
                       $activity_result['tariffs_id'],
                       $activity_result['activities_travel_distance'],
+                      $activity_result['activities_travel_description'],
                       $activity_result['activities_expenses'],
                       $activity_result['activities_ticket_number'],
                       $activity_result['activities_comment'],
@@ -57,6 +68,8 @@
           return $this->tariff->unit->name;
         case 'travel_distance':
           return $this->travel_distance;
+        case 'travel_description':
+          return $this->travel_description;
         case 'expenses':
           return $this->expenses;
         case 'ticket_number':
@@ -69,18 +82,18 @@
       return null;
     }
 
-    public static function get_array($timesheet_id = 0, $sort_order = 'activities_date asc') {
+    public static function get_array($timesheet_id = 0, $sort_order = 'activities_date ASC') {
       $database = $_SESSION['database'];
       $activity_array = array();
 
       if (tep_not_null($timesheet_id)) {
         $index = 0;
         $timesheet_id = $database->prepare_input($timesheet_id);
-        $activities_query = $database->query("select activities_id from " . TABLE_ACTIVITIES . " a" .
-                                             " inner join (" . TABLE_PROJECTS . " p, " . TABLE_ROLES . " r, " . TABLE_EMPLOYEES_ROLES . " er, " . TABLE_TARIFFS . " t)" . 
-                                             " on (p.projects_id=r.projects_id and r.roles_id=er.roles_id and er.employees_roles_id=t.employees_roles_id and t.tariffs_id=a.tariffs_id)" .
-                                             " where a.timesheets_id = '" . (int)$timesheet_id . "'" .
-                                             " order by " . $sort_order);
+        $activities_query = $database->query("SELECT activities_id FROM " . TABLE_ACTIVITIES . " a " .
+                                             "INNER JOIN (" . TABLE_PROJECTS . " p, " . TABLE_ROLES . " r, " . TABLE_EMPLOYEES_ROLES . " er, " . TABLE_TARIFFS . " t) " . 
+                                             "ON (p.projects_id=r.projects_id AND r.roles_id=er.roles_id AND er.employees_roles_id=t.employees_roles_id AND t.tariffs_id=a.tariffs_id) " .
+                                             "WHERE a.timesheets_id = '" . (int)$timesheet_id . "' " .
+                                             "ORDER BY " . $sort_order . ";");
         while ($activities_result = $database->fetch_array($activities_query)) {
           $activity_array[$index] = new activity($activities_result['activities_id']);
           $index++;
@@ -90,11 +103,12 @@
       return $activity_array;
     }
 
-    public function fill($date = 0, $amount = 0, $tariffs_id = '', $travel_distance = 0, $expenses = 0, $ticket_number = '', $comment = '', $timesheet_id = 0) {
+    public function fill($date = 0, $amount = 0, $tariffs_id = '', $travel_distance = 0, $travel_description = '', $expenses = 0, $ticket_number = '', $comment = '', $timesheet_id = 0) {
       $this->date = $date;
       $this->amount = $this->format('amount', $amount);
       $this->tariff = new tariff($tariffs_id);
       $this->travel_distance = $this->format('travel_distance', $travel_distance);
+      $this->travel_description = $travel_description;
       $this->expenses = $this->format('expenses', $expenses);
       $this->ticket_number = $ticket_number;
       $this->comment = $comment;
@@ -107,7 +121,9 @@
 
       if (tep_not_null($parents_id)) {
         $index = 0;
-        $activities_query = $database->query("select activities_id from " . TABLE_ACTIVITIES . " where tariffs_id in (" . $parents_id . ") and " . $column_name1 . " " . $comparison1 . " '" . $value1 . "'" . (tep_not_null($delimiter)?' ' . $delimiter . " " . $column_name2 . " " . $comparison2 . " '" . $value2 . "'":''));
+        $activities_query = $database->query("SELECT activities_id " .
+                                             "FROM " . TABLE_ACTIVITIES . " " .
+                                             "WHERE tariffs_id IN (" . $parents_id . ") AND " . $column_name1 . " " . $comparison1 . " '" . $value1 . "'" . (tep_not_null($delimiter)?' ' . $delimiter . " " . $column_name2 . " " . $comparison2 . " '" . $value2 . "'":'') . ";");
         while ($activities_result = $database->fetch_array($activities_query)) {
           $activities_array[$index] = new activity($activities_result['activities_id']);
           $index++;
@@ -183,17 +199,17 @@
       // Insert a new activity if one does not exist and retrieve the id
       if ($this->activity_id == 0) {
         // The activity does not exist
-        $database->query("insert into " . TABLE_ACTIVITIES . " (activities_date, activities_amount, tariffs_id, activities_travel_distance, activities_expenses, activities_ticket_number, activities_comment, timesheets_id) values ('" . tep_strftime(DATE_FORMAT_DATABASE, $this->date) . "', '" . $this->amount . "', '" . $this->tariff->id . "', '" . $this->travel_distance . "', '" . $this->expenses . "', '" . $this->ticket_number . "', '" . $database->input($this->comment) . "', '" . $this->timesheet_id . "')");
+        $database->query("INSERT INTO " . TABLE_ACTIVITIES . " (activities_date, activities_amount, tariffs_id, activities_travel_distance, activities_travel_description, activities_expenses, activities_ticket_number, activities_comment, timesheets_id) values ('" . tep_strftime(DATE_FORMAT_DATABASE, $this->date) . "', '" . $this->amount . "', '" . $this->tariff->id . "', '" . $this->travel_distance . "', '" . $database->input($this->travel_description) . "', '" . $this->expenses . "', '" . $this->ticket_number . "', '" . $database->input($this->comment) . "', '" . $this->timesheet_id . "');");
         $this->activity_id = $database->insert_id(); // The proper id is now known
       } else {
         // The activity exists, update the contents
-        $activity_query = $database->query("update " . TABLE_ACTIVITIES . " set activities_date='" . tep_strftime(DATE_FORMAT_DATABASE, $this->date) . "', activities_amount='" . $this->amount . "', tariffs_id='" . $this->tariff->id . "', activities_travel_distance='" . $this->travel_distance . "', activities_expenses='" . $this->expenses . "', activities_ticket_number='" . $this->ticket_number . "', activities_comment='" . $database->input($this->comment) . "' where activities_id = '" . (int)$this->activity_id . "'");
+        $activity_query = $database->query("UPDATE " . TABLE_ACTIVITIES . " SET activities_date='" . tep_strftime(DATE_FORMAT_DATABASE, $this->date) . "', activities_amount='" . $this->amount . "', tariffs_id='" . $this->tariff->id . "', activities_travel_distance='" . $this->travel_distance . "', activities_travel_description='" . $database->input($this->travel_description) . "', activities_expenses='" . $this->expenses . "', activities_ticket_number='" . $this->ticket_number . "', activities_comment='" . $database->input($this->comment) . "' where activities_id = '" . (int)$this->activity_id . "';");
       }
     }
 
     public function delete() {
       $database = $_SESSION['database'];
-      $activity_query = $database->query("delete from " . TABLE_ACTIVITIES . " where activities_id = '" . (int)$this->activity_id . "'");
+      $activity_query = $database->query("DELETE FROM " . TABLE_ACTIVITIES . " WHERE activities_id = '" . (int)$this->activity_id . "';");
       // Reset id, otherwise one might think this activity (still) exists in db
       $this->activity_id = 0;
     }
@@ -202,12 +218,14 @@
       $db_date = tep_strftime(DATE_FORMAT_DATABASE, $selected_date);
       $database = $_SESSION['database'];
 
-      $activity_query = $database->query("SELECT a.activities_id, a.tariffs_id" .
-                                         " FROM " . TABLE_ACTIVITIES . " AS a, " . TABLE_TIMESHEETS . " AS t" .
-                                         " WHERE a.timesheets_id = t.timesheets_id" . 
-                                         " AND t.employees_id = '" . $employee_id . "'" .
-                                         " AND a.activities_date < '" . $db_date . "'" .
-                                         " ORDER BY a.activities_date DESC, a.activities_id DESC");
+      $activity_query = $database->query("SELECT a.activities_id, " .
+                                           "a.tariffs_id " .
+                                         "FROM " . TABLE_ACTIVITIES . " AS a, " .
+                                           TABLE_TIMESHEETS . " AS t " .
+                                         "WHERE a.timesheets_id = t.timesheets_id " . 
+                                           "AND t.employees_id = '" . $employee_id . "' " .
+                                           "AND a.activities_date < '" . $db_date . "' " .
+                                         "ORDER BY a.activities_date DESC, a.activities_id DESC;");
       $activity_result = $database->fetch_array($activity_query);
 
       if (tep_not_null($activity_result)) {
@@ -231,11 +249,11 @@
                                            " AND t.tariffs_start_date <= '" . $db_date . "'" .
                                            " AND t.tariffs_end_date >= '" . $db_date . "'"); */
         // The easy way: just validate against the obtained tariffs_id
-        $activity_query = $database->query("SELECT 1" .
-                                           " FROM " . TABLE_TARIFFS . " AS t" .
-                                           " WHERE t.tariffs_id = '" . $tariffs_id . "'" .
-                                           " AND t.tariffs_start_date <= '" . $db_date . "'" .
-                                           " AND t.tariffs_end_date >= '" . $db_date . "'");
+        $activity_query = $database->query("SELECT 1 " .
+                                           "FROM " . TABLE_TARIFFS . " AS t " .
+                                           "WHERE t.tariffs_id = '" . $tariffs_id . "' " .
+                                             "AND t.tariffs_start_date <= '" . $db_date . "' " .
+                                             "AND t.tariffs_end_date >= '" . $db_date . "';");
         $activity_result = $database->fetch_array($activity_query);
         if (tep_not_null($activity_result)) {
           // Activity is valid for given employee and date
